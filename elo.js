16e747b84803022ef7f4763fc6990a35bdc71968
Bartechaosp <69191597+Bartechaosp@users.noreply.github.com>
2023-03-22 15:45:07 +0100

    Add files via upload

--- elo.js
+++ elo.js
@@ -12,22 +12,114 @@
 
 // zad 2
 
-const tab = [1,2,3,4,5,6,7,8,9,0]
+// const tab = [1,2,3,4,5,6,7,8,9,0]
 
-const tabsort = (tablica) => {
-    if (tablica[0] > tablica[1]) {
-        tablica.sort((a, b) => a - b)
-        for (element of tablica) {
-            if (element == 0) element = 8
-            console.log(element)
-        }
-    } else if (tablica[0] < tablica[1]) {
-        tablica.sort((a,b) => b - a)
-        for (element of tablica) {
-            if (element == 0) element = 8
-            console.log(element)
-        }
+// const tabsort = (tablica) => {
+//     if (tablica[0] > tablica[1]) {
+//         tablica.sort((a, b) => a - b)
+//         for (element of tablica) {
+//             if (element == 0) element = 8
+//             console.log(element)
+//         }
+//     } else if (tablica[0] < tablica[1]) {
+//         tablica.sort((a,b) => b - a)
+//         for (element of tablica) {
+//             if (element == 0) element = 8
+//             console.log(element)
+//         }
+//     }
+// }
+
+// tabsort(tab)
+
+//zad 3
+
+// const deleteLastTab = (tablica) => {
+//     const newArray = []
+//     for(let i = 0; i<(tablica.length) ;i++) {
+//        if (!isNaN(tablica[i]) && tablica[i] !== 0 && tablica[i] !== null && tablica[i] !== false && tablica[i] !== true && tablica[i] !== undefined) {
+//         newArray.push(tablica[i])
+//        }
+//     }
+//     console.log(newArray)
+// }
+// const tablica = [NaN, 0, true, 1, false, -21, '',undefined, 37, null]
+// deleteLastTab(tablica)
+
+//zad 4
+
+// const addAnElement = (tablica ,...elementy) => {
+    
+//     tablica.push(...elementy)
+//     return tablica
+// }
+
+// const tablica = [1,2,3,4]
+
+// console.log(addAnElement(tablica, "a","b","c","d"))
+
+//zad 5
+
+// const myfunction = (tablica) => {
+//     let newArray = [tablica[0]]
+//     let pow = 0
+//     for (let z = 0; z < tablica.length ;z++) {
+//             for (let i = z + 1; i < (tablica.length -1);i++) {
+//                 if (tablica[z] == tablica[i] && newArray.includes(tablica[z])){
+//                     pow++
+//                     newArray.push(tablica[z])
+//                 }
+//             }
+//     }
+//     console.log(pow)
+// }
+
+// const tab = [1,1,2,1,2,5,3,3,3]
+
+// myfunction(tab)
+
+//zad 6
+
+const suma = (sum1, sum2, sum3, sum4) => {
+
+}
+
+const jakstam = (tab1, tab2, tab3, tab4) => {
+    let sum1 = 0
+    let sum2 = 0
+    let sum3 = 0
+    let sum4 = 0
+    let sum = 0
+    for (let i = 0; i < tab1.length ;i++) {
+        sum1 += tab1[i]
+    }
+
+    for (let i = 0; i < tab2.length ;i++) {
+        sum2 += tab2[i]
+    }
+
+    for (let i = 0; i < tab3.length ;i++) {
+        sum3 += tab3[i]
+    }
+
+    for (let i = 0; i < tab4.length ;i++) {
+        sum4 += tab4[i]
+    }
+    
+    if (sum1 > (sum2 + sum3 + sum4)) {
+        return sum1
+    } else if (sum2 > (sum1 + sum2 + sum3)) {
+        return sum2
+    } else if (sum3 > (sum1 + sum2 + sum4)) {
+        return sum3
+    } else {
+        return sum4
     }
 }
 
-tabsort(tab)
+const tab1 = [1,1,1,1]
+const tab2 = [1,1,1,2]
+const tab3 = [1,1,1,3]
+const tab4 = [1,1,1,4]
+
+console.log(jakstam(tab1, tab2, tab3, tab4))
