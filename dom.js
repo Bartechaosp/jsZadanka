fc630ad0c84e05a2aa4bf46fe6515cdb617e5791
Bartechaosp <69191597+Bartechaosp@users.noreply.github.com>
2023-04-12 14:28:56 +0200

    Create dom.js

---
+++ dom.js
@@ -0,0 +1,32 @@
+// zad1
+
+// const newDiv = document.createElement("div");
+// const ebody = document.querySelector("body");
+// newDiv.innerHTML = "elowina";
+// ebody.appendChild(newDiv);
+
+// zad2
+
+// const myh1 = document.getElementsByTagName("h1")
+// myh1[0].style.color="red"
+
+//zad3
+
+// const myImg = document.getElementsByTagName("img")[0].setAttribute("src", "elo")
+
+//zad4
+
+// const myP = document.getElementsByTagName("p")[0].style.display="none"
+
+//Zad5
+
+// const newClassToDiv = document.getElementsByTagName("div")[0].setAttribute("class", "eloe")
+
+//Zad6
+
+//const myUl = document.getElementsByTagName("ul li")
+//myUl[0].setAttribute("class", "elo")
+
+//const elWithAct = document.querySelector("ul")
+
+
